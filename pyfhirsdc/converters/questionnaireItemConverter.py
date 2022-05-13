37bdd5dafcd5333ee61c80be60d8d57619b1df42
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2022-05-13 12:51:32 +0200

    making sure an error is display when type is missing

--- pyfhirsdc/converters/questionnaireItemConverter.py
+++ pyfhirsdc/converters/questionnaireItemConverter.py
@@ -240,7 +240,9 @@ def get_question_definition(question):
 
 def get_type_details(question):
     # structure main_type detail_1::detail_2
-    type_arr = question['type'].split(" ")
+    if 'type' not in question or not isinstance(question['type'], str):
+        return None, None, None
+    type_arr = str(question['type']).split(" ")
     # split each details
     if len(type_arr)>1:
         detail_arr = type_arr[1].split('::')
