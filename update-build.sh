74ed3c841ee20f0539576b51783bc4b1001bd55d
delcroip <patrick.delcroix@swisstph.ch>
2023-01-23 15:03:41 +0100

    automatic build number

---
+++ update-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+oldnum=$(cut -d '=' -f2 build.txt)
+newnumber=$(expr $oldnum + 1)
+sed -i "s/$oldnum\$/$newnumber/g" ./build.txt
+sed -i "s/\.build.*\"/.build.$newnumber\"/g" ./conf.json
