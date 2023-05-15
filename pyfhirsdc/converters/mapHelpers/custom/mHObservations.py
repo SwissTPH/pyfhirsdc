e59a1491b617cbcaf6a3257de4574bf36e755157
delcroip <patrick.delcroix@swisstph.ch>
2023-05-15 13:55:48 +0200

    fix structureMap SetObservationValueSetStr

--- pyfhirsdc/converters/mapHelpers/custom/mHObservations.py
+++ pyfhirsdc/converters/mapHelpers/custom/mHObservations.py
@@ -32,9 +32,9 @@ def get_obs_valueset_str_rules(df_valueset):
     for index, concept in df_valueset.iterrows():
         rules_main.append(
             MappingRule(
-            expression = "a where value = '{}', a.value as val".format(concept['display']),
+            expression = "a where value = '{}'".format(concept['display']),
             rules = [ MappingRule(
-                expression="val -> tgt.value = create('CodeableConcept') as cc, cc.coding = create('Coding') as c, c.code='{}', c.system= '{}', tgt.status = 'final'".format(concept['code'], get_custom_codesystem_url())
+                expression="a -> tgt.value = create('CodeableConcept') as cc, cc.coding = create('Coding') as c, c.code='{}', c.system= '{}', tgt.status = 'final'".format(concept['code'], get_custom_codesystem_url())
             )])
         )
         
@@ -84,6 +84,15 @@ def get_obs_value_rules(question_id, df_questions_item,none_name):
     )])])]
             
     
+def SetObservationCodeObs(mode, profile, question_id,df_questions_item, *args):
+    if mode == 'groups':
+        none_name = args[0] if len(args)>1 else 'none'
+        code =  question_id
+        rule_name = clean_group_name(question_id)
+        return [set_generic_observation_v2( profile, rule_name, code, get_obs_value_rules(code,df_questions_item, none_name))]
+    elif mode == 'docs':
+        return get_base_obs_docs(question_id, 'Boolean', df_questions_item)    
+
 
 
 
@@ -202,26 +211,33 @@ def get_obs_bool_code_rules(question_id, df_questions_item):
  ####### SetObservationMultiple :  works only with valueset, will generate an obs for all, cancelled is not selected but for the one with value none   ###### 
 # args[0] : valueSet name
 def SetObservationMultiple(mode, profile, question_id, df_questions, *args):
-    if len(args)!= 1:
-        logger.error('SetObservation must have 1 parameters')
-        return None
-    df_valueset = get_valueset_df(args[0], True)   
-    if mode == 'rules':
-        return get_base_obs_muli_rules(profile, question_id,df_questions,df_valueset)
-    elif mode == 'groups':
-        return get_base_obs_muli_groups(profile, question_id,df_valueset)
-    elif mode == 'docs':
-        return get_docs_obs_muli(question_id,df_valueset,  df_questions)
+    return SetObservationMultipleBase(mode, profile, question_id, df_questions, False, *args)
     
+def SetObservationMultipleConcat(mode, profile, question_id, df_questions, *args):
+    return SetObservationMultipleBase(mode, profile, question_id, df_questions, True, *args)
+    
+def SetObservationMultipleBase(mode, profile, question_id, df_questions, concat, *args):   
+    if len(args)!= 1 and  len(args)!= 2 :
+        logger.error('SetObservation must have 1 or 2 parameters')
+        return None
+    df_valueset = get_valueset_df(args[0], True)
+    none_code  = args[1] if  len(args) == 2 else None
+    if mode == 'rules':
+        return get_base_obs_muli_rules(profile, question_id,df_questions,df_valueset, none_code, concat)
+    elif mode == 'groups':
+        return get_base_obs_muli_groups(profile, question_id,df_valueset, none_code,concat)
+    elif mode == 'docs':
+        return get_docs_obs_muli(question_id,df_valueset,  df_questions, none_code,concat)
 
-def get_docs_obs_muli(question_id,df_valueset, df_questions_item):
+
+def get_docs_obs_muli(question_id,df_valueset, df_questions_item, none_code, concat = True):
     docs = []
     for index, row in df_valueset.iterrows():
-        if "map" in row and   pd.notna(row["map"]) and row['map'].lower().startswith('obs'):
+        if "map" in row and   pd.notna(row["map"]) and row['map'].lower().startswith('obs') and row['code'] != none_code:
             docs.append(
                 {
                     'type' : 'Observation',
-                    'code' : question_id+ "&" +  row['code'],
+                    'code' : question_id+ "&" +  row['code'] if concat else row['code'],
                     'valueType' : 'boolean',
                     'description': '{}:{}'.format(
                         df_questions_item[df_questions_item.id==question_id].iloc[0]['label'],
@@ -234,44 +250,45 @@ def get_docs_obs_muli(question_id,df_valueset, df_questions_item):
     
         
 
-def get_base_obs_muli_rules(profile, question_id,df_questions,df_valueset):
+def get_base_obs_muli_rules(profile, question_id,df_questions,df_valueset, none_code = None, concat = True):
     rules = []
- 
-    
+
     for index, row in df_valueset.iterrows():
         if "map" in row and   pd.notna(row["map"]) and row['map'].lower().startswith('obs'):
-            row_id = row['code']
-            code  =question_id+ "&" +  row_id
-            rule_name = clean_group_name( profile + code + 't')
-            rules.append(MappingRule(
-                expression = "src where src.item.where(linkId='{0}').answer.where(value.code = '{1}') ".format(question_id, row_id),
-                rules = [wrapin_entry_create( profile, question_id,df_questions,[MappingRule(expression = 'src then {}(src,tgt)'.format(rule_name) )])]
-            ))
             
-            rule_name = clean_group_name( profile + code+ 'f')
-            rules.append(MappingRule(
-                expression = "src where src.item.where(linkId='{0}').exists() and src.item.where(linkId='{0}').answer.where(value.code = '{1}').empty()  ".format(question_id, row_id),
-                rules = [wrapin_entry_create(profile, question_id,df_questions, [MappingRule(expression = 'src then {}(src,tgt)'.format(rule_name) )])]
-            ))
+            row_id = row['code']
+            if none_code is None or  row_id !=  none_code:
+                code  =question_id+ "&" +  row_id if concat else row_id
+                rule_name = clean_group_name( profile + code + 't')
+                rules.append(MappingRule(
+                    expression = "src where src.item.where(linkId='{0}').answer.where(value.code = '{1}') ".format(question_id, row_id),
+                    rules = [wrapin_entry_create( profile, question_id,df_questions,[MappingRule(expression = 'src then {}(src,tgt)'.format(rule_name) )])]
+                ))
+                
+                rule_name = clean_group_name( profile + code+ 'f')
+                rules.append(MappingRule(
+                    expression = "src where src.item.where(linkId='{0}').exists() and src.item.where(linkId='{0}').answer.where(value.code = '{1}').empty()  ".format(question_id, row_id),
+                    rules = [wrapin_entry_create(profile, question_id,df_questions, [MappingRule(expression = 'src then {}(src,tgt)'.format(rule_name) )])]
+                ))
             
     return  rules
 
 
-def get_base_obs_muli_groups(profile, question_id,df):
+def get_base_obs_muli_groups(profile, question_id,df,none_code = None, concat = True):
     groups = []
-    
     for index, row in df.iterrows():
         if "map" in row and   pd.notna(row["map"]) and row['map'].lower().startswith('obs'):
             row_id = row['code']
-            code  =question_id+ "&" +  row_id
-            rule_name = clean_group_name( profile + code + 't')
-            groups.append(
-                set_generic_observation_v2(profile, rule_name, code, [MappingRule(expression = "src -> tgt.status = 'final', tgt.value = true")],'t')
-            )
-            rule_name = clean_group_name( profile + code+ 'f')
-            groups.append(
-                set_generic_observation_v2(profile, rule_name, code, [MappingRule(expression = "src -> tgt.status = 'cancelled',tgt.value = false",)],'f')
-            )
+            if none_code is None or  row_id !=  none_code:
+                code  =question_id+ "&" +  row_id if concat else row_id
+                rule_name = clean_group_name( profile + code + 't')
+                groups.append(
+                    set_generic_observation_v2(profile, rule_name, code, [MappingRule(expression = "src -> tgt.status = 'final', tgt.value = true")],'t')
+                )
+                rule_name = clean_group_name( profile + code+ 'f')
+                groups.append(
+                    set_generic_observation_v2(profile, rule_name, code, [MappingRule(expression = "src -> tgt.status = 'cancelled',tgt.value = false",)],'f')
+                )
     return groups        
 
 def set_generic_observation_v2(profile, rule_name, code ,spe_rules, sufix = ''):
@@ -292,3 +309,4 @@ def set_generic_observation_v2(profile, rule_name, code ,spe_rules, sufix = ''):
             *spe_rules
         ]
     )
+    
