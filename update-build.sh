b3ea1b52fc92a39c0c0d0e16f659720c9c3b32fe
delcroip <patrick.delcroix@swisstph.ch>
2023-07-21 12:51:50 +0200

    misc

--- update-build.sh
+++ update-build.sh
@@ -2,4 +2,4 @@
 oldnum=$(cut -d '=' -f2 build.txt)
 newnumber=$(expr $oldnum + 1)
 sed -i "s/$oldnum\$/$newnumber/g" ./build.txt
-sed -i "s/\.build.*\"/.build.$newnumber\"/g" ./conf.json
+sed -i "s/\.build.*\"/.build.$newnumber\"/g" ./conf*.json
